4ec9410746fa3bd010c35f435a83137baedcb7d5
Christian Farris <christianfarriscs@gmail.com>
2022-04-25 22:56:23 -0500

    feat: default user

---
+++ constants/user-constants.js
@@ -0,0 +1,17 @@
+const defaultUser = {
+  createdAt: '2016-09-24T20:38:40Z',
+  name: 'Christian Farris',
+  login: 'chrstnfrrs',
+  avatarUrl:
+    'https://avatars.githubusercontent.com/u/22419884?u=1d8d4b6139715f487196c7992df17d691d28d7f9&v=4',
+  bio: "Vue.js fan who's passionate about empathetic design and accessibility",
+  company: 'Vertalo',
+  location: 'Austin, TX',
+  websiteUrl: 'christianfarris.com',
+  twitterUsername: 'chrstnfrrs',
+  followers: { totalCount: 10 },
+  following: { totalCount: 65 },
+  repositories: { totalCount: 35 },
+};
+
+export { defaultUser };
