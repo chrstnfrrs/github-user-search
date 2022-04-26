ce2d9a3960da959ad9035bb64ea6487eccf1189e
Christian Farris <christianfarriscs@gmail.com>
2022-04-25 22:56:18 -0500

    feat: useFetch hook

---
+++ hooks/useFetch.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { $fetch } from 'ohmyfetch';
+
+const useFetch = (url, { params = {} }) => {
+  const [loading, setLoading] = React.useState(true);
+  const [data, setData] = React.useState(undefined);
+  const [error, setError] = React.useState(false);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const res = await $fetch(url, {
+        params,
+      });
+  
+      setError(res.error);
+      setData(res.data);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return {
+    data,
+    error,
+    loading,
+    refetch: fetchData,
+  };
+};
+
+export default useFetch;
