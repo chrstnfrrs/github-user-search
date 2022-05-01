127b12f924c41abd05070a78a49d62a49d5507ea
Christian Farris <christianfarriscs@gmail.com>
2022-04-30 23:57:19 -0500

    test: netlify functions

--- pages/api/search.js
+++ pages/api/search.js
@@ -24,6 +24,7 @@ const USER_SEARCH_QUERY = `query ($username: String!) {
 }`;
 
 const handler = async (req, res) => {
+  console.log('searching...')
   const { data } = await $fetch('https://api.github.com/graphql', {
     method: 'POST',
     headers: {
